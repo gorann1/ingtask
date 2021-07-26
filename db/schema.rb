8d5aad81178bf8f06a60feba0101e7a596f950ac
goran <goran.novacic@gmail.com>
2021-07-26 20:13:24 +0200

    devise_guard__add_card_info_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_223133) do
+ActiveRecord::Schema.define(version: 2021_07_26_180937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_223133) do
     t.string "card_exp_month"
     t.string "card_exp_year"
     t.datetime "expires_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
